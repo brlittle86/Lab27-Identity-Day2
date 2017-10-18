db58bbaa58062918cccf37cab59d0bbb35795df2
Brandon Little <brlittle86@gmail.com>
2017-10-18 08:07:43 -0700

    created home controller and view, created characters controller with views and CRUD operations

--- DnDManager/DnDManager/Startup.cs
+++ DnDManager/DnDManager/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using DnDManager.Models;
 
 namespace DnDManager
 {
@@ -23,6 +25,9 @@ namespace DnDManager
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<DnDManagerContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("DnDManagerContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
